1741e6641c3a9aa1c061ab15e868911d07673260
Joseph Zhao <zhaozihan542259545@gmail.com>
2025-02-16 22:44:39 +0800

    update

--- exercises/easy/algorithm1.rs
+++ exercises/easy/algorithm1.rs
@@ -65,10 +65,40 @@ impl<T> LinkedList<T> {
             },
         }
     }
-    pub fn merge(list_a: LinkedList<T>, list_b: LinkedList<T>) -> LinkedList<T> {
-        let mut l_merged = &mut list_a;
-        while list_b.start.is_some() {}
-        list_a
+    pub fn merge(list_a: LinkedList<T>, list_b: LinkedList<T>) -> LinkedList<T>
+    where
+        T: PartialOrd + Copy,
+    {
+        let mut result = LinkedList::new();
+        let mut a_ptr = list_a.start;
+        let mut b_ptr = list_b.start;
+
+        while let (Some(a_node), Some(b_node)) = (a_ptr, b_ptr) {
+            let a_val = unsafe { &(*a_node.as_ptr()).val };
+            let b_val = unsafe { &(*b_node.as_ptr()).val };
+
+            if a_val <= b_val {
+                result.add(*a_val);
+                a_ptr = unsafe { (*a_node.as_ptr()).next };
+            } else {
+                result.add(*b_val);
+                b_ptr = unsafe { (*b_node.as_ptr()).next };
+            }
+        }
+
+        while let Some(a_node) = a_ptr {
+            let a_val = unsafe { &(*a_node.as_ptr()).val };
+            result.add(*a_val);
+            a_ptr = unsafe { (*a_node.as_ptr()).next };
+        }
+
+        while let Some(b_node) = b_ptr {
+            let b_val = unsafe { &(*b_node.as_ptr()).val };
+            result.add(*b_val);
+            b_ptr = unsafe { (*b_node.as_ptr()).next };
+        }
+
+        return result;
     }
 }
 
